2663ea22b5e0b636feb58a25f92b354d272daa6f
Tarek.Talaat <Tarek.Talaat@qsitint.com>
2025-07-10 17:47:23 +0300

    TBC

--- Src/helpers/config.py
+++ Src/helpers/config.py
@@ -1,4 +1,14 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+'''
+This module defines the applications configuration settings using Pydantic.
+It uses environment variables to set the configuration values.
+This allows for easy management of application settings and ensures that
+the application can be configured without changing the codebase.
+It also provides a function to retrieve the settings instance.
+This is useful for accessing configuration values throughout the application.
+'''
+from pydantic_settings import BaseSettings, SettingsConfigDict # This library is used for managing application settings
+# SETTINGSConfigDict is used to define the configuration for the settings class
+# BaseSettings is the base class for creating settings classes with Pydantic
 class Settings(BaseSettings):
     APP_NAME: str
     APP_VERSION: str
--- Src/routes/base.py
+++ Src/routes/base.py
@@ -1,8 +1,15 @@
+'''
+This module defines the base router for the FastAPI application.
+it will be loaded by the main application and will handle the root endpoint.
+'''
 from fastapi import FastAPI, APIRouter, Depends
 import os
 from helpers.config import get_settings, Settings
 
-base_router = APIRouter()
+base_router = APIRouter(
+    prefix = "/api/v1",
+    tags = ["api_v1"],
+)
 
 @base_router.get("/")
 async def welcome(app_settings: Settings = Depends(get_settings)):
